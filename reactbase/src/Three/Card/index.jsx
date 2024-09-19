af643cdc7164a2251b5fc2d6956ea3f9d5fa3a3e
kavinkumar10 <kavinkumar.prof@gmail.com>
2024-09-19 11:07:58 +0530

    ui update

--- reactbase/src/Three/Card/index.jsx
+++ reactbase/src/Three/Card/index.jsx
@@ -4,9 +4,14 @@ import { AiOutlineLike,AiOutlineDislike } from "react-icons/ai";
 
 const Card = ({post}) =>{
     return(
-        <div className="Card bg-slate-400 p-5 rounded-md flex flex-col gap-5">
+        <div className="Card bg-slate-400 text-slate-900 p-5 rounded-md flex flex-col gap-5">
             <h1 className="text-3xl font-bold">{post.title}</h1>
-            <p>{post.body}</p>
+            <p className="opacity-80">{post.body}</p>
+        
+            <div className="flex gap-5">
+                <button className="border border-slate-600 bg-slate-400 rounded flex px-3 py-1 items-center justify-center gap-2"><AiOutlineLike/> {post.reactions.likes}</button>
+                <button className="border border-slate-600 bg-slate-400 rounded flex px-3 py-1 items-center justify-center gap-2"><AiOutlineDislike/>{post.reactions.dislikes}</button>
+            </div>
             {
                 post.userId === 123?
                 <div className="flex justify-between">
@@ -14,11 +19,6 @@ const Card = ({post}) =>{
                     <button className="text-slate-50 font-bold px-3 py-2 rounded-md bg-slate-800">Edit</button>
                 </div>:<></>
             }
-            <div className="flex gap-5">
-                <button className="border border-slate-600 bg-slate-400 rounded flex px-3 py-1 items-center justify-center gap-2"><AiOutlineLike/> {post.reactions.likes}</button>
-                <button className="border border-slate-600 bg-slate-400 rounded flex px-3 py-1 items-center justify-center gap-2"><AiOutlineDislike/>{post.reactions.dislikes}</button>
-            </div>
-
         </div>
     )
 }
