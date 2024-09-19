0f5c8ac7469bf5fe68ede1328b9bb9308a68b695
kavinkumar10 <kavinkumar.prof@gmail.com>
2024-09-19 10:58:28 +0530

    likes and dislike updated

--- reactbase/src/Three/Card/index.jsx
+++ reactbase/src/Three/Card/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { AiOutlineLike,AiOutlineDislike } from "react-icons/ai";
+
 
 const Card = ({post}) =>{
     return(
@@ -6,12 +8,17 @@ const Card = ({post}) =>{
             <h1 className="text-3xl font-bold">{post.title}</h1>
             <p>{post.body}</p>
             {
-                post.userId == "123"?
+                post.userId === 123?
                 <div className="flex justify-between">
                     <button className="text-slate-50 font-bold px-3 py-2 rounded-md bg-red-900">Delete</button>
                     <button className="text-slate-50 font-bold px-3 py-2 rounded-md bg-slate-800">Edit</button>
                 </div>:<></>
             }
+            <div className="flex gap-5">
+                <button className="border border-slate-600 bg-slate-400 rounded flex px-3 py-1 items-center justify-center gap-2"><AiOutlineLike/> {post.reactions.likes}</button>
+                <button className="border border-slate-600 bg-slate-400 rounded flex px-3 py-1 items-center justify-center gap-2"><AiOutlineDislike/>{post.reactions.dislikes}</button>
+            </div>
+
         </div>
     )
 }
